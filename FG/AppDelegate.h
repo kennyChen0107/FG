0d87d8951ddbe2c4b80551a383fce7cae54b4e7e
Kenny <kenny@nb164teki-MacBook-2.local>
2015-03-06 15:26:13 +0800

    Initial Commit

---
+++ FG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FG
+//
+//  Created by Kenny on 2015/3/6.
+//  Copyright (c) 2015年 FG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FG
+//
+//  Created by Kenny on 2015/3/6.
+//  Copyright (c) 2015年 FG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
